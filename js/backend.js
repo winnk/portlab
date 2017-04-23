2fa9c2fee38e04a1d562b189be63541c69975c20
Kaya Winn <mail@psyreal.net>
2017-04-23 12:51:48 -0700

    refactored code after bash subsystem and typo fix

--- js/backend.js
+++ js/backend.js
@@ -1,4 +1,9 @@
 var apiKey = require('./../.env').apiKey;
+// var apiPre =
+// var apiIssue =
+// var apiPost =
+
+
 
 // var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=cardiologist&location=or-portland&skip=2&limit=10&user_key=faf8804b327e1667c939d01e9d89bb10' + apiKey;
 
@@ -12,7 +17,19 @@ function Doctor(){
 
 //VARIABLE SEARCH STRING 'https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=' + specialty_uid + '&location' + location  + '&skip=2&limit=10&user_key=' + apiKey
 
-Bike.prototype.getSpecialtyLocation = function(location, displaySpecialty) {
+
+// exports.getDoctors = function(medicalIssue) {
+//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+//    .then(function(result) {
+//       console.log(result);
+//     })
+//    .fail(function(error){
+//       console.log("fail");
+//     });
+// };
+
+
+Doctor.prototype.getSpecialtyLocation = function(location, displaySpecialty) {
   console.log("specialty runs");
  $.get('https://api.betterdoctor.com/2016-03-01/practices?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key='+ apiKey).then(function(response) {
         var allDocs = [];
@@ -31,9 +48,9 @@ Bike.prototype.getSpecialtyLocation = function(location, displaySpecialty) {
         });
         displaySpecialtyLocation(allDocs);
       })
-      .fail(function(error) {
-       $('.showDocs').text(error.responseJSON.message);
-     });
+      .fail(function(error){
+	      $('#error').text(error.responseJSON.message);
+	    });
 };
 
 exports.docModule = Doctor;
--- js/interface.js
+++ js/interface.js
@@ -1,19 +1,29 @@
 var Doctor = require('./../js/backend.js').docModule;
-var apiKey = require('./../.env').apiKey;
-
+// var apiKey = require('./../.env').apiKey;
+//
 function displaySpecialtyLocation(docs){
   docs.forEach(function(doc){
     $('.showDocs').append("<a href=https://api.betterdoctor.com/2016-03-01/doctors/" + npi + "?user_key=" + apiKey + ">Doctor: </a>" + doc.title + doc.name + "<br>");
   });
   }
 
-// VARIABLE SEARCH STRING"<span><a href=https://api.betterdoctor.com/2016-03-01/doctors/" + npi + "?user_key=faf8804b327e1667c939d01e9d89bb10"
-
-// SEARCH BY NPI https://api.betterdoctor.com/2016-03-01/doctors/npi/1992773295?user_key=faf8804b327e1667c939d01e9d89bb10
+// // VARIABLE SEARCH STRING"<span><a href=https://api.betterdoctor.com/2016-03-01/doctors/" + npi + "?user_key=faf8804b327e1667c939d01e9d89bb10"
+//
+// // SEARCH BY NPI https://api.betterdoctor.com/2016-03-01/doctors/npi/1992773295?user_key=faf8804b327e1667c939d01e9d89bb10
 
 
 $(document).ready(function() {
-  var currentDocObject = new Doc();
+  var currentDocObject = new Doctor();
+  exports.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=cancer&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key= faf8804b327e1667c939d01e9d89bb10')
+   .then(function(result) {
+      console.log(result);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+};
+
   $('#doctorSearch').click(function() {
     console.log("search triggered");
     // event.preventDefault();
