99b003c17a27f84af0ee2471c66b06ff1e07fb32
Kaya Winn <mail@psyreal.net>
2017-04-26 07:22:22 -0700

    fixes backend logic to display docs on front end by symptom search

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,23 +1,22 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 var getDoctors = require('./../js/doctor.js').getDoctors;
 
-var displayDoctors = function(symptom, doctor) {
+var currentDoc = new Doctor();
+
+function displayDoctors(doctors) {
   console.log("displayDoctors run");
   doctors.forEach(function(doctor) {
-        $('#showDocs').append('<tr class="warning">'+
-                          "<td>doctor.data.practices.name</td>"+
-                        '</tr>');
+    $('#showDocs').append('<span>' + doctor.first_name + '<span><br>');
   });
 };
 
 $(document).ready(function() {
-  var currentDoc = new Doctor();
   $('#symptomSearch').submit(function(){
     console.log("search triggered");
     event.preventDefault();
     var symptom = $('#symptom').val();
-    $('#symptom').val('');
-    console.log(symptom);
+    // $('#symptom').val('');
+    console.log(symptom + "is symptom entered");
     currentDoc.getDoctors(symptom, displayDoctors);
     }); // end search
  }); // end ready function
--- js/doctor.js
+++ js/doctor.js
@@ -1,18 +1,29 @@
 var apiKey = require('./../.env').apiKey;
 var apiPre = "https://api.betterdoctor.com/2016-03-01/doctors?";
 var apiPost = "&location=37.773,-122.413,100&skip=2&limit=10&user_key=";
-var medicalIssue = "headache";
+var medicalIssue = "cancer";
 
-function Doctor(){
+function Doctor() {
 }
 
-exports.getDoctors = function(issue, displayDoctors) {
+Doctor.prototype.getDoctors = function(issue, displayDoctors) {
   console.log("getDoctor runs");
- $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=headache&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=faf8804b327e1667c939d01e9d89bb10').then(function(response) {
-// console.log(JSON.stringify(response));
-    displayDoctors(symptom, response.data.practices.name);
+ $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=cancer&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=faf8804b327e1667c939d01e9d89bb10').then(function(response) {
+   // console.log(JSON.stringify(response));
+   var doctors = [];
+   response.data.forEach(function(doctor) {
+     console.log("forEach runs");
+   var docObject = {};
+   docObject.first_name = doctor.profile.first_name;
+   docObject.last_name = doctor.profile.last_name;
+   docObject.drtitle = doctor.profile.title;
+   console.log("allDoc pushed");
+   doctors.push(docObject);
+ });
+      console.log("display docs runs docObject")
+     displayDoctors(doctors);
   })
-  .fail(function(error){
+    .fail(function(error){
     $('#error').text(error.responseJSON.message);
   });
 };
