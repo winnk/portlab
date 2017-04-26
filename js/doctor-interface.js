38bb4f39646f8d0d1d4df79efb233f134098e168
Kaya Winn <mail@psyreal.net>
2017-04-25 21:37:47 -0700

    refactor code and adds console.logs for debugging

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,20 +1,23 @@
-var Doctor = require('./../js/doctor.js').docModule;
+var Doctor = require('./../js/doctor.js').doctorModule;
 var getDoctors = require('./../js/doctor.js').getDoctors;
 
-function displayDoctors(issue, doctor){
-  doctors.forEach(function(doctor){
-    $('.showDocs').append('<p> doctor.data.practices.name</p>');
+var displayDoctors = function(symptom, doctor) {
+  console.log("displayDoctors run");
+  doctors.forEach(function(doctor) {
+        $('#showDocs').append('<tr class="warning">'+
+                          "<td>doctor.data.practices.name</td>"+
+                        '</tr>');
   });
 };
 
 $(document).ready(function() {
-  var currentDoc= new Doctor();
-  $('#symptomSearch').submit(function() {
+  var currentDoc = new Doctor();
+  $('#symptomSearch').submit(function(){
     console.log("search triggered");
     event.preventDefault();
-    var issue = $('#symptom').val;
+    var symptom = $('#symptom').val();
     $('#symptom').val('');
-    console.log(issue);
-    currentDoc.getDoctors(issue, displayDoctors);
+    console.log(symptom);
+    currentDoc.getDoctors(symptom, displayDoctors);
     }); // end search
  }); // end ready function
--- js/doctor.js
+++ js/doctor.js
@@ -1,50 +1,20 @@
 var apiKey = require('./../.env').apiKey;
 var apiPre = "https://api.betterdoctor.com/2016-03-01/doctors?";
 var apiPost = "&location=37.773,-122.413,100&skip=2&limit=10&user_key=";
+var medicalIssue = "headache";
 
 function Doctor(){
 }
 
-Doctor.prototype.getDoctors = function(issue, displayDoctors) {
+exports.getDoctors = function(issue, displayDoctors) {
   console.log("getDoctor runs");
- $.get('https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=cardiologist&location=or-portland&skip=2&limit=10&user_key=faf8804b327e1667c939d01e9d89bb10').then(function(response) {
-    displayDoctors(issue, response.data.practices.name);
+ $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=headache&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=faf8804b327e1667c939d01e9d89bb10').then(function(response) {
+// console.log(JSON.stringify(response));
+    displayDoctors(symptom, response.data.practices.name);
   })
-   .fail(function(error){
-	  $('#error').text(error.responseJSON.message);
+  .fail(function(error){
+    $('#error').text(error.responseJSON.message);
   });
 };
 
-exports.docModule = Doctor;
-
-
-// exports.getDoctors = function(issue, displayDoctors) {
-//   console.log("getDoctor runs");
-//  $.get(apiPre + issue + apiPost + apiKey).then(function(response) {
-//     displayDoctors(issue, response.data.practices.name);
-//   })
-//    .fail(function(error){
-// 	  $('#error').text(error.responseJSON.message);
-//   });
-// };
-
-// var resource_url = apiPre + issue + apiPost + apiKey; 'https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=cardiologist&location=or-portland&skip=2&limit=10&user_key=faf8804b327e1667c939d01e9d89bb10' + apiKey;
-
-
-// SEARCH BY SPECIALTY
-// 'https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=' + specialty_uid + apiPost + apiKey
-
-// SEARCH BY PRACTICIES IN CITY https://api.betterdoctor.com/2016-03-01/practices?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=faf8804b327e1667c939d01e9d89bb10
-
-//VARIABLE SEARCH STRING 'https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=' + specialty_uid + '&location' + location  + '&skip=2&limit=10&user_key=' + apiKey
-
-
-// exports.getDoctors = function(medicalIssue) {
-//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-//    .then(function(result) {
-//       console.log(result);
-//     })
-//    .fail(function(error){
-//       console.log("fail");
-//     });
-// };
+exports.doctorModule = Doctor;
