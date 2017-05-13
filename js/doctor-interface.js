b5da9b5ef016ef3333abae78f70d60846e80d9ae
Kaya Winn <mail@psyreal.net>
2017-05-13 16:33:55 -0700

    add minor styling updates and fixes photo alignments.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -5,16 +5,16 @@ function Display(result) {
   $('#symptom').empty();
   $('#showDocs').empty();
   result.forEach(function(doc) {
-    $('#showDocs').append("<li class='well row'><div class='col-md-2'><img id="docThumb" src =" + doc.profile.image_url +
-      "></div> <div id="profile" class='col-md-8'><strong>Name:</strong> " + doc.profile.first_name +
+    $('#showDocs').append("<div class='procard'><li><div id='profile' class='col-md-8'><strong>Name:</strong> " + doc.profile.first_name +
       " " + doc.profile.last_name + ", " + doc.profile.title +
       "<br /> <strong>Specialty:</strong> " + doc.specialties[0].name +
       "<br><strong>Practice:</strong> " + doc.practices[0].name +
       "<br><strong>Practice phone:</strong> " + doc.practices[0].phones[0].number +
       "<br><strong>Practice address:</strong> " + doc.practices[0].visit_address.street +
       ", " + doc.practices[0].visit_address.city + " " + doc.practices[0].visit_address.state +
-      " <br / ><p></p><strong>Bio:</strong> " + doc.profile.bio +
-      "</div></li>");
+      " <br /><p></p><strong>Bio:</strong> " + doc.profile.bio +
+      "</div><div id='image' class='col-md-1'><img src =" + doc.profile.image_url +
+        "></div></li></div>");
 
   });
 }
