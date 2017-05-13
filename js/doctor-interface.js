5eae4077fee9ddcd3b6d85d06e80e1bd160b51b9
Kaya Winn <mail@psyreal.net>
2017-05-13 12:09:48 -0700

    updates styling

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,31 +1,30 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
-// var getDoctors = require('./../js/doctor.js').getDoctors;
 var currentDoc = new Doctor();
 
-function Display(result){
+function Display(result) {
   $('#symptom').empty();
   $('#showDocs').empty();
-  result.forEach(function(doc){
+  result.forEach(function(doc) {
     $('#showDocs').append("<li class='well row'><div class='col-md-2'><img src =" + doc.profile.image_url +
-    "></div> <div class='col-md-10'><strong>Name:</strong> " + doc.profile.first_name +
-    " " + doc.profile.last_name + ", " + doc.profile.title +
-    "<br /> <strong>Specialty:</strong> " + doc.profile.specialty +
-    "<br><strong>Practice:</strong> " + doc.practices[0].name +
-    "<br><strong>Practice phone:</strong> " + doc.practices[0].phones[0].number +
-    "<br><strong>Practice address:</strong> " + doc.practices[0].visit_address.street +
-    ", " + doc.practices[0].visit_address.city + " " + doc.practices[0].visit_address.state +
-    " <br><strong>Bio:</strong> " + doc.profile.bio +
-    "</div></li>");
+      "></div> <div class='col-md-8'><strong>Name:</strong> " + doc.profile.first_name +
+      " " + doc.profile.last_name + ", " + doc.profile.title +
+      "<br /> <strong>Specialty:</strong> " + doc.specialties[0].name +
+      "<br><strong>Practice:</strong> " + doc.practices[0].name +
+      "<br><strong>Practice phone:</strong> " + doc.practices[0].phones[0].number +
+      "<br><strong>Practice address:</strong> " + doc.practices[0].visit_address.street +
+      ", " + doc.practices[0].visit_address.city + " " + doc.practices[0].visit_address.state +
+      " <br / ><p></p><strong>Bio:</strong> " + doc.profile.bio +
+      "</div></li>");
 
   });
 }
 
 $(document).ready(function() {
-  $('#symptomSearch').submit(function(){
+  $('#symptomSearch').submit(function() {
     $('#showDocs').empty();
     var location = $('#location').val();
     var symptom = $('#symptom').val();
     event.preventDefault();
     currentDoc.getDoctors(symptom, location, Display);
-      });
-    });
+  });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -3,8 +3,6 @@ var apiPre = "https://api.betterdoctor.com/2016-03-01/doctors?query=";
 var apiLocation = "&location=";
 var apiPost = "&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=";
 
-
-
 function Doctor() {}
 
 Doctor.prototype.getDoctors = function(symptom, location, Display) {
